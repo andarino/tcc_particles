adc5f07bfe107451ddc4fa2023ea1d0021057b94
andar1n0 <tj.vinci97@gmail.com>
2022-02-11 18:25:08 -0300

    updating

--- autotestes.sh
+++ autotestes.sh
@@ -14,7 +14,12 @@ if [ -z $1 ]; then
 	exit 0
 fi 
 
-if [ -e downloading_wall_restitution_GPU1 ]
+if [ -e downloading_wall_restitution_GPU0 ]
+then 
+	echo "[!] removendo arquivo downloading_wall_restitution_GPU1 existente "
+	rm downloading_wall_restitution_GPU0
+
+elif [ -e downloading_wall_restitution_GPU1 ]
 then 
 	echo "[!] removendo arquivo downloading_wall_restitution_GPU1 existente "
 	rm downloading_wall_restitution_GPU1
@@ -32,7 +37,9 @@ else
 	echo  "[x] Nenhum executável para ser removido !!!"
 fi
 
-echo "[!] compilando -O1, -O2, -O3 com os parametro: $*"
+echo "[!] compilando -O0, -O1, -O2, -O3 com os parametro: $*"
+
+OPT0=`gcc downloading_wall_restitution_GPU.c -o downloading_wall_restitution_GPU0 $* -O0`
 
 OPT1=`gcc downloading_wall_restitution_GPU.c -o downloading_wall_restitution_GPU1 $* -O1`
 
@@ -43,11 +50,24 @@ OPT3=`gcc downloading_wall_restitution_GPU.c -o downloading_wall_restitution_GPU
 test -d "time" || mkdir -m 700 time
 
 #***************************************************************
+echo "[!] executando o programa 10 vezes com a flag 'O0' "
+echo "[!] Aguarde ... "
+
+echo " "
+echo "PARAMETROS USADOS: $*" > time/gcctimeO0.txt
+for i in $(seq 10);
+ do
+  ./downloading_wall_restitution_GPU0 0.0001 8 10 0.09 >> time/gcctimeO0.txt
+	echo "=============================================================" >> time/gcctimeO0.txt
+  sleep 5
+done
+echo "[v] tempos salvos em 'time/gcctimeO0'"
+#***********************************************************
 echo "[!] executando o programa 10 vezes com a flag 'O1' "
 echo "[!] Aguarde ... "
 
-echo "PARAMETROS USADOS: $*" > time/gcctimeO1.txt
 echo " "
+echo "PARAMETROS USADOS: $*" > time/gcctimeO1.txt
 
 for i in $(seq 10);
  do
@@ -60,6 +80,7 @@ echo "[v] tempos salvos em 'time/gcctimeO1'"
 #***************************************************************
 echo "[!] executando o programa 10 vezes com flag 'O2' "
 echo "[!] Aguarde ... "
+echo " "
 
 echo "PARAMETROS USADOS: $*" > time/gcctimeO2.txt
 echo " "
